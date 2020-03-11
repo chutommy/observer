4b232e960326e6efa2153f74ef5a0925bf6ed2f1
Tommy Chu <tommychu2256@gmail.com>
2020-03-11 20:13:38 +0100

    adding test

--- app/todo.go
+++ app/todo.go
@@ -6,6 +6,4 @@ package main
 //		setServo	real time changing
 // 					waiting and calculating time with the speed
 //		centerServo	100% wait
-//		make program which would center and test servos independently
-// 		add more cascades
 // 		degree according to horizontal and vertical
---
+++ tester/main.go
@@ -0,0 +1,74 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"time"
+
+	piblaster "github.com/ddrager/go-pi-blaster"
+)
+
+var servos = piblaster.Blaster{}
+
+const pin1 = 17
+const pin2 = 18
+
+var center, calibrate, test bool
+
+func main() {
+	servos.Start([]int64{pin1, pin2})
+
+	time.Sleep(600 * time.Millisecond)
+
+	if center {
+		fmt.Println("Centering servos...")
+		moveServo(90, 390)
+	}
+
+	if calibrate {
+		moveServo(90, 390)
+
+		moveServo(0, 390)
+
+		moveServo(180, 780)
+
+		moveServo(90, 390)
+	}
+
+	if test {
+		moveServo(90, 390)
+		moveServo(70, 250)
+		moveServo(110, 300)
+		moveServo(50, 350)
+		moveServo(130, 400)
+		moveServo(30, 450)
+		moveServo(150, 500)
+		moveServo(10, 550)
+		moveServo(170, 600)
+		moveServo(0, 650)
+		moveServo(180, 700)
+		moveServo(90, 400)
+	}
+
+	fmt.Println("ABOUT TO EXIT ...")
+}
+
+func init() {
+	fmt.Printf("TESTING SERVOS with GPIO pins: %v, %v\n", pin1, pin2)
+	cnt := (flag.Bool("center", false, "center both servos"))
+	clb := (flag.Bool("calibrate", false, "calibrate both servos"))
+	tst := (flag.Bool("test", false, "test the servos"))
+
+	flag.Parse()
+
+	center = *cnt
+	calibrate = *clb
+	test = *tst
+}
+
+func moveServo(angle, mils int) {
+	a := float64(angle)/900 + 0.05
+	servos.Apply(pin1, a)
+	servos.Apply(pin2, a)
+	time.Sleep(time.Duration(mils) * time.Millisecond)
+}
