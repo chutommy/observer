f14b580edf8b64f4e35d23adda710ce0b07bf4d7
Tommy Chu <tommychu2256@gmail.com>
2020-06-19 14:16:17 +0200

    add install.sh script

---
+++ install.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/bash
+
+# Install Observer project and all dependencies with go
+
+# gobot
+go get -d -u gobot.io/x/gobot/...
+if [ $? -ne "0" ]
+then
+    echo "Could not get Gobot"
+fi
+
+# gocv and opencv
+go get -u -d gocv.io/x/gocv
+if [ $? -ne "0" ]
+then
+    echo "Could not get GoCV"
+fi
+
+cd $GOPATH/src/gocv.io/x/gocv
+if [ $? -ne "0" ]
+then
+    echo "GOPATH variable not set"
+fi
+
+make install
+if [ $? -ne "0" ]
+then
+    echo "Could not install OpenCV"
+fi
+
+# observer
+go get github.com/chutified/observer
+if [ $? -ne "0" ]
+then
+    echo "Could not get Observer"
+fi
