5768bc6afb9f5d86bc05445a827114e072313fe7
chutified <tommychu2256@gmail.com>
2021-01-08 23:14:27 +0100

    Add general movements to the arsenal of Servo activities

--- engine/common.go
+++ engine/common.go
@@ -1,1 +1,45 @@
 package engine
+
+import (
+	"time"
+)
+
+// Center centres the Servo to 90 degree.
+func (s *Servo) Center() {
+	s.set(90)
+}
+
+// Center centres a duo of Servo.
+func (ss *ServoXY) Center() {
+	ss.servoX.set(90)
+	ss.servoY.set(90)
+}
+
+// Calibrate tries the range for the Servo.
+func (s *Servo) Calibrate() {
+	s.set(minDegree)
+	time.Sleep(800 * time.Millisecond)
+	s.set(maxDegree)
+	time.Sleep(800 * time.Millisecond)
+}
+
+// Calibrate calibrates both Servo.
+func (ss *ServoXY) Calibrate() {
+	ss.Center()
+	time.Sleep(400 * time.Millisecond)
+
+	// try minDegree
+	ss.servoX.set(minDegree)
+	time.Sleep(400 * time.Millisecond)
+	ss.servoY.set(minDegree)
+	time.Sleep(400 * time.Millisecond)
+
+	// try maxDegree
+	ss.servoX.set(maxDegree)
+	time.Sleep(800 * time.Millisecond)
+	ss.servoY.set(maxDegree)
+	time.Sleep(800 * time.Millisecond)
+
+	ss.Center()
+	time.Sleep(400 * time.Millisecond)
+}
