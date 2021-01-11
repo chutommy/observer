46b42192b7a173eba45898d160acbb0328006d44
chutified <tommychu2256@gmail.com>
2021-01-11 21:12:12 +0100

    Remove init function: customization is solved in another way

--- initiation.go
+++ initiation.go
@@ -1,9 +1,7 @@
 package main
 
 import (
-	"flag"
 	"image"
-	"time"
 
 	piblaster "github.com/ddrager/go-pi-blaster"
 )
@@ -18,56 +16,6 @@ var tolerationX float64
 var tolerationY float64
 var tolerationXr float64
 var tolerationYr float64
-
-func init() {
-	csc1 := flag.String("casc1", cascade1, "path to cascade")
-	csc2 := flag.String("casc2", cascade2, "path to cascade (optional)")
-	srvx := flag.Int64("servox", servoXpin, "GPIO pin of servoX (not the pin number)")
-	srvy := flag.Int64("servoy", servoYpin, "GPIO pin of servoY (not the pin number)")
-	cams := flag.Int("camsource", cameraSource, "camera source")
-	camw := flag.Int("camwidth", camWidth, "camera width")
-	camh := flag.Int("camheight", camHeight, "camera height")
-	aovhor := flag.Float64("angleovh", angleOfViewHor, "camera's horizontal angle of view")
-	aovver := flag.Float64("angleovv", angleOfViewVer, "camera's vertical angle of view")
-	mfps := flag.Int64("maxfps", int64(maxFPS), "camera's max FPS")
-	prd := flag.Int64("period", int64(period), "speed of the loop (ns)")
-	aima := flag.Float64("aimarea", aimArea, "aim area (1~100%)")
-	idldur := flag.Float64("idledur", idleDuration, "time until reset (sec)")
-	calib := flag.Bool("calib", calibration, "calibration on start")
-	invx := flag.Bool("invertx", invertX, "invert X")
-	invy := flag.Bool("inverty", invertY, "invert Y")
-	calibx := flag.Float64("calibx", calibrateX, "calibrate X (1~100%)")
-	caliby := flag.Float64("caliby", calibrateY, "calibrate Y (1~100%)")
-	tx := flag.Float64("tolx", tolerateX, "toleration of X (1~100%)")
-	ty := flag.Float64("toly", tolerateY, "toleration of Y (1~100%)")
-	// window enable
-	wnd := flag.Bool("window", windowed, "enable window (ONLY IF DISPLAY OUTPUT IS AVAILABLE) - ensure VNC or HDMI output")
-	flag.Parse()
-
-	cascade1 = *csc1
-	cascade2 = *csc2
-	servoXpin = *srvx
-	servoYpin = *srvy
-	cameraSource = *cams
-	camWidth = *camw
-	camHeight = *camh
-	angleOfViewHor = *aovhor
-	angleOfViewVer = *aovver
-	maxFPS = time.Duration(*mfps)
-	period = time.Duration(*prd)
-	aimArea = *aima
-	idleDuration = *idldur
-	calibration = *calib
-	invertX = *invx
-	invertY = *invy
-	calibrateX = *calibx
-	calibrateY = *caliby
-	tolerateX = *tx
-	tolerateY = *ty
-	windowed = *wnd
-
-	resetVar()
-}
 
 func resetVar() {
 
