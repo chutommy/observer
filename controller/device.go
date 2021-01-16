742392f3632a195082667b2902477af689396c9f
chutified <tommychu2256@gmail.com>
2021-01-16 11:09:50 +0100

    Implement an initialization of the Observer robot

---
+++ controller/device.go
@@ -0,0 +1,30 @@
+package controller
+
+import (
+	"gobot.io/x/gobot"
+)
+
+// LoadRobot loads all required devices for the Rebot.
+func (o *Observer) LoadRobot() {
+	// define adaptors and devices
+	conns := gobot.Connections{o.adaptor}
+	devices := gobot.Devices{o.camera}
+
+	// add window if Show is enabled
+	if o.cfg.Show {
+		devices = append(devices, o.window)
+	}
+
+	// init robot
+	o.robot = gobot.NewRobot(
+		o.name,
+		conns,
+		devices,
+		o.work,
+	)
+}
+
+// Start starts the Observer.
+func (o *Observer) Start() error {
+	return o.robot.Start()
+}
