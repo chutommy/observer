668b7762230566e069b32c335135adc8a3bec6f8
chutified <tommychu2256@gmail.com>
2021-01-16 11:15:11 +0100

    Replace main.go functionalities with the observer package

--- main.go
+++ main.go
@@ -2,45 +2,15 @@ package main
 
 import (
 	"errors"
-	"fmt"
 	"log"
-	"sync/atomic"
-	"time"
 
-	piblaster "github.com/ddrager/go-pi-blaster"
-	"gobot.io/x/gobot"
-	"gobot.io/x/gobot/platforms/opencv"
-	"gobot.io/x/gobot/platforms/raspi"
-	"gocv.io/x/gocv"
 	"observer/config"
-	"observer/engine"
-	"observer/geometry"
-	"observer/observerconfig"
+	"observer/controller"
 )
 
 const robotName = "Observer"
 
-// frames
-var img atomic.Value
-
-// idle variables
-var idleTime = time.Now()
-var idleStatus = false
-var centered = false
-
-var servos = piblaster.Blaster{}
-
-// raspberry pi adaptor
-var raspiAdaptor = raspi.NewAdaptor()
-
-// showing camera's view
-var window *opencv.WindowDriver
-
-// camera driver
-var camera = opencv.NewCameraDriver(cameraSource)
-
 func main() {
-
 	// load configuration
 	cfg, err := config.GetConfig(".")
 	if err != nil {
@@ -51,145 +21,20 @@ func main() {
 		}
 	}
 
-	// load observer configuration
-	ocfg := observerconfig.LoadObserverConfig(cfg)
+	// constructs and config the new Observer
+	o := controller.NewObserver(robotName, cfg)
+	o.LoadWork()
 
-	// enable window driver
-	if ocfg.Show {
-		window = opencv.NewWindowDriver()
+	// starts the observer
+	err = o.Start()
+	if err != nil {
+		log.Fatal("failed to start the Observer: %w", err)
 	}
-
-	servos.Start([]int64{servoXpin, servoYpin})
-
-	// robot's func
-	work := func() {
-
-		// creating a storage for frames
-		mat := gocv.NewMat()
-		defer mat.Close()
-		img.Store(mat)
-
-		// turn camera on
-		camera.On(opencv.Frame, func(data interface{}) {
-			i := data.(gocv.Mat)
-			// store the frame
-			img.Store(i)
-		})
-
-		// initialize servos
-		servoX := engine.NewServo(servos, ocfg.ServoX)
-		servoY := engine.NewServo(servos, ocfg.ServoY)
-		servoXY := engine.NewServos(servoX, servoY)
-
-		// init center
-		servoXY.CenterMiddleUp()
-		time.Sleep(381 * time.Millisecond)
-
-		// calibrate servos if enabled
-		if ocfg.CalibrateOnStart {
-			servoXY.Calibrate()
-		}
-
-		// limit period according to maxFPS
-		if 1000/period > maxFPS {
-			reduced := (1000 / maxFPS) + 1
-			log.Printf("Reducing period from %v to %v (according to max. FPS) ...", period, reduced)
-			period = reduced
-		}
-
-		time.Sleep(2 * time.Second)
-		log.Println("Observing start running ...")
-
-		// main loop every {period}ns
-		gobot.Every(period*time.Millisecond, func() {
-
-			//load the frame from img
-			i := img.Load().(gocv.Mat)
-			if i.Empty() {
-				return
-			}
-
-			// scan for objects (query the given haar cascades)
-			objects := make(geometry.Objects, 0)
-			for _, haar := range ocfg.Haar {
-				rects := opencv.DetectObjects(haar, i)
-				objects = append(objects, geometry.FromRects(rects)...)
-			}
-
-			// get the target's index and rectangle
-			targetX := geometry.NearestObject(objects)
-			target := objects[targetX]
-
-			if targetX != -1 {
-
-				// draw the target
-				target.Draw(&i, ocfg.Colors.Target.ToRGBA(), ocfg.Colors.Target.T())
-
-				// draw non-target objects
-				otherObjects := append(objects[:targetX], objects[(targetX+1):]...)
-				otherObjects.Draw(&i, ocfg.Colors.Other.ToRGBA(), ocfg.Colors.Other.T())
-
-				// reset idle and suspend the counter
-				if idleStatus {
-					idleStatus = false
-				}
-
-				// get a target's coordinate
-				lock := target.Center()
-
-				// aim the target if it is not in the middle rectangle
-				if !lock.In(ocfg.MidRect) {
-					servoXY.Aim(lock)
-				}
-
-			} else {
-
-				// set new idleStatus
-				if !idleStatus {
-					idleTime = time.Now()
-					idleStatus = true
-					centered = false
-
-				} else if !centered {
-
-					// get the time difference, if idle too long - reset
-					if time.Now().Sub(idleTime).Seconds() >= ocfg.MaxIdleDuration {
-						fmt.Println("Idle to long ...")
-						servoXY.Center()
-						centered = true
-					}
-				}
-			}
-
-			// show window
-			if ocfg.Show {
-
-				// draw a mid rect
-				geometry.FromRect(ocfg.MidRect).Draw(&i, ocfg.Colors.MidRect.ToRGBA(), ocfg.Colors.MidRect.T())
-
-				window.ShowImage(i)
-				window.WaitKey(1)
-			}
-
-		})
-	}
-
-	// define adaptors and devices
-	connections := []gobot.Connection{raspiAdaptor}
-	devices := []gobot.Device{camera}
-
-	// adds window if window is enabled
-	if ocfg.Show {
-		devices = append(devices, window)
-	}
-
-	// set robot atributes
-	robot := gobot.NewRobot(
-		robotName,
-		connections,
-		devices,
-		work,
-	)
-
-	robot.Start()
 }
+
+// TODO
+// - add logger
+// - refactor readme file
+// - code cleanup (dependencies, haar cascades etc.)
+// - Docker implementation
+// - install sh
